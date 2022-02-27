308cafc0730c63ff32b3d63374e3a2ed6a894eaa
Jack Daniels <jack@lassogames.com>
2022-02-27 11:43:54 -0500

    Added tests for standard items. 6 tests passing, handled cases for Sulfuras items and Standard, just need Brie, backstage passes, and conjured.

--- lib/item_updater.ex
+++ lib/item_updater.ex
@@ -3,5 +3,5 @@ defmodule ItemUpdater do
   def get("Aged Brie"), do: GildedRose.Item.AgedBrie
   def get("Backstage passes"), do: GildedRose.Item.BackstagePasses
   def get("Conjured"), do: GildedRose.Item.Conjured
-  def get(_name), do: GildedRose.Item.Regular
+  def get(_name), do: GildedRose.Item.Standard
 end
--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,6 +1,37 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
+  describe "when standard item" do
+    test "decrease quality and sell_in by 1" do
+      fixed = [%GildedRose.Item{name: "Regular item", sell_in: 9, quality: 10}]
+      expected = [%GildedRose.Item{name: "Regular item", sell_in: 8, quality: 9}]
+
+      assert GildedRose.update_quality(fixed) == expected
+    end
+
+    test "don't decrease quality if it is already 0 when sell_in is positive" do
+      fixed = [%GildedRose.Item{name: "Regular item", sell_in: 9, quality: 0}]
+      expected = [%GildedRose.Item{name: "Regular item", sell_in: 8, quality: 0}]
+
+      assert GildedRose.update_quality(fixed) == expected
+    end
+
+    test "don't decrease quality if it is already 0 when sell_in is 0 or less" do
+      fixture = [%GildedRose.Item{name: "Regular item", sell_in: 0, quality: 0}]
+      expected = [%GildedRose.Item{name: "Regular item", sell_in: -1, quality: 0}]
+
+      assert GildedRose.update_quality(fixture) == expected
+    end
+
+    test "quality decreases by 2 when sell_in is 0 or less" do
+      fixture = [%GildedRose.Item{name: "Regular item", sell_in: 0, quality: 5}]
+      expected = [%GildedRose.Item{name: "Regular item", sell_in: -1, quality: 3}]
+
+      assert GildedRose.update_quality(fixture) == expected
+    end
+  end
+
+
   describe "when item is 'Sulfuras'" do
     test "quality or sell_in doesn't change" do
       fixed = [%GildedRose.Item{name: "Sulfuras", sell_in: 9, quality: 10}]
